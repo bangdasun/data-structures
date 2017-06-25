c15fbb7e77d981f341ed920535e49c80ca8932f2
Bangda Sun <bangdasun94@gmail.com>
2017-06-25 18:05:33 -0400

    Update sort-selection.py

--- python-implementation/sort-selection.py
+++ python-implementation/sort-selection.py
@@ -1,14 +1,18 @@
 def selectionSort(lst):
     """
+    selectionSort(lst) -> sorted lst with ascend order
+    
     https://www.tutorialspoint.com/data_structures_algorithms/selection_sort_algorithm.htm
     
-    Step 1 − Set MIN to location 0
-    Step 2 − Search the minimum element in the list
-    Step 3 − Swap with value at location MIN
-    Step 4 − Increment MIN to point to next element
-    Step 5 − Repeat until list is sorted
+    (1) Set MIN to location 0
+    (2) Search the minimum element in the list, THIS IS THE MAIN POINT
+    (3) Swap with value at location MIN
+    (4) Increment MIN to point to next element
+    (5) Repeat until list is sorted
     
-    selectionSort(lst) -> sorted lst with ascend order
+    The algorithm makes sure that the element before minloc is sorted,
+    the algorithm will search the MINIMUM value at unsorted part (use loop and compare one by one, it's a 'greedy idea')
+    
     """
     # set basic parameters
     lstsize = len(lst)
@@ -20,14 +24,17 @@ def selectionSort(lst):
         
         # search for the minimum value start from minimum location
         for j in range(minloc, lstsize):
-            # find the location of value that small than minimum location
+            # find the location of value that small than minimum location one by one, 
+            # the left part of the minloc will all greater than lst[minloc]
             if sortedlst[minloc] > sortedlst[j]:
                 minloc = j
         
         # swap the current element to the element at minimum location
         # the current element will be the minimum 
-        sortedlst[minloc], sortedlst[i] = sortedlst[i], sortedlst[minloc]
-    
+        if minloc != i:
+            sortedlst[minloc], sortedlst[i] = sortedlst[i], sortedlst[minloc]
+            print('swap ' + str(sortedlst[minloc]) + ' and ' + str(sortedlst[i]) + \
+                  ', list is ' + str(sortedlst))
     return sortedlst
 
 selectionSort([9, 8, 7, 6, 5, 4, 3, 2, 1])
